82d14160c5f75b7926afb298d1164ffc6137977a
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-08-03 14:20:05 +0530

    Added AI Agent Class

--- SlnSemanticKernelAI/SemanticKernelAI/Program.cs
+++ SlnSemanticKernelAI/SemanticKernelAI/Program.cs
@@ -2,6 +2,9 @@
 using SemanticKernelCore.Connectors;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using SemanticKernelCore.Connectors.Ollama;
+using Microsoft.SemanticKernel.Services;
+using SemanticKernelCore.Connectors.Configuration;
 
 namespace SemanticKernelAI
 {
@@ -18,7 +21,19 @@ namespace SemanticKernelAI
             //var app = builder.Build();
             // Resolve and use your services as needed
 
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            IKernelService kernelService = new KernelService();
+            kernelService.CreatekernelBuilder();
+
+            IChatCompletionConnector chatCompletionConnector = new OllamaConnector(kernelService);
+            OllamaConnectorChatCompletionConfig ollamConfig = new OllamaConnectorChatCompletionConfig();
+
+            ollamConfig.ModelId = "llama3.1:latest";
+            ollamConfig.Uri = "http://localhost:11434/";    
+            chatCompletionConnector.AddChatCompletion(ollamConfig);
+
+            kernelService.BuildKernel();
+
         }
     }
 }
