cd5fee9dfa90b1e095fc7a50926b30e8bbad621e
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-08-23 14:51:35 +0530

    Added Qdrantvectorstore

---
+++ SlnSemanticKernelAI/SemanticKernelCore/VectorStoreCore/QdrantVector/QdrantVectorStoreService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.AI;
+using Microsoft.SemanticKernel.Connectors.Qdrant;
+using Qdrant.Client;
+
+namespace SemanticKernelCore.VectorStoreCore.QdrantVector
+{
+    public class QdrantVectorStoreService : IVectorStoreService
+    {
+        private readonly QdrantVectorStore? _vectorStore;
+        private readonly QdrantCollection<ulong, DataLoader.DataContent>? _collection;
+        public QdrantVectorStoreService(IEmbeddingGenerator embeddingGenerator, string uri, string apikey, string collectionname)
+        {
+            QdrantClient qdrantClient = new QdrantClient(new Uri(uri), apikey);
+
+            _vectorStore = new QdrantVectorStore(qdrantClient, true,
+               new QdrantVectorStoreOptions
+               {
+                   EmbeddingGenerator = embeddingGenerator,
+               });
+            _collection = _vectorStore.GetCollection<ulong, DataLoader.DataContent>(collectionname);
+
+            _collection.EnsureCollectionExistsAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task UpSert(IEnumerable<DataLoader.DataContent> dataContents)
+        {
+            var data = dataContents.Select(snippet => new DataLoader.DataContent
+            {
+                Key = snippet.Key,
+                Text = snippet.Text, // You can modify the text as needed
+            });
+
+            await _collection.UpsertAsync(data);
+        }
+        public QdrantCollection<ulong, DataLoader.DataContent>? Collection => _collection;
+    }
+}
