9406da32d5f743b4d7c4e0c3c91d679ad952cd89
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-09-06 17:51:00 +0530

    Added MCP Server

---
+++ SlnSemanticKernelAI/MCPServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using PizzaOrderPlugin.Plugin;
+using SemanticKernelCore.KernelCore;
+using SemanticKernelCore.McpServer;
+using SemanticKernelCore.Plugin;
+
+//////// Kernel and Plugin Initialization ////////
+IKernelService kernelService = new KernelService();
+kernelService.CreatekernelBuilder();
+
+IPLuginObject pLuginObject = new PLuginObject();
+pLuginObject.KernelService = kernelService;
+
+List<Object> Plugins = new List<object>();
+Plugins.Add(new PizzaPlugin());
+pLuginObject.AddPluginObject(Plugins);
+
+kernelService.BuildKernel();
+
+//////// ASP.NET Core Web Application ////////  
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMcpServer()
+    .WithHttpTransport()
+    .WithTools(kernelService.Kernel.Plugins);
+
+var app = builder.Build();
+
+app.MapMcp();
+app.Run();
---
+++ SlnSemanticKernelAI/SemanticKernelCore/McpServer/McpServerBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.Extensions.DependencyInjection;
+using ModelContextProtocol.Server;
+
+namespace SemanticKernelCore.McpServer
+{
+    public static class McpServerBuilderExtensions
+    {
+        public static IMcpServerBuilder WithTools(this IMcpServerBuilder builder, KernelPluginCollection plugins)
+        {
+            foreach (var plugin in plugins)
+            {
+                foreach (var function in plugin)
+                {
+                    builder.Services.AddSingleton(services => McpServerTool.Create(function));
+                }
+            }
+            return builder;
+        }
+    }
+}
