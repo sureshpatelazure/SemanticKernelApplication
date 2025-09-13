a2e1809625b71ab0be9ffd8d496c7637c7b4baa6
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-09-13 16:49:14 +0530

    Added QdrantVectorStoreConnector

---
+++ SlnSemanticKernelAI/SemanticKernelCore/Connectors/IVectorStoreConnector.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.SemanticKernel.Connectors.Qdrant;
+using SemanticKernelCore.Connectors.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemanticKernelCore.Connectors
+{
+    public interface IVectorStoreConnector
+    {
+        public QdrantVectorStore AddVectorStore<T>(T connectorConfiguration, IEmbeddingGenerator embeddingGenerator) where T : IAIConnectorConfiguration;    
+    }
+}
--- SlnSemanticKernelAI/SemanticKernelCore/Connectors/VectorStore/QdrantVectorStoreConnector.cs
+++ SlnSemanticKernelAI/SemanticKernelCore/Connectors/VectorStore/QdrantVectorStoreConnector.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.AI;
+using Microsoft.SemanticKernel.Connectors.Qdrant;
+using Qdrant.Client;
+using SemanticKernelCore.Connectors.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,18 @@ using System.Threading.Tasks;
 
 namespace SemanticKernelCore.Connectors.VectorStore
 {
-    internal class QdrantVectorStoreConnector
+    public class QdrantVectorStoreConnector : IVectorStoreConnector
     {
+        public QdrantVectorStore AddVectorStore<T>(T connectorConfiguration, IEmbeddingGenerator embeddingGenerator  ) where T : IAIConnectorConfiguration
+        {
+            QdrantVectorStorConfiguration qdrantVectorStorConfiguration = connectorConfiguration as QdrantVectorStorConfiguration;
+            QdrantClient qdrantClient = new QdrantClient(new Uri(qdrantVectorStorConfiguration.Uri), qdrantVectorStorConfiguration.ApiKey);
+
+             return new QdrantVectorStore(qdrantClient, true,
+               new QdrantVectorStoreOptions
+               {
+                   EmbeddingGenerator = embeddingGenerator,
+               });
+        }
     }
 }
